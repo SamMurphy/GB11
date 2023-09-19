0c4e80ac0d8ad12d486afe7d63e4decf655415b4
Holly <honeill31@gmail.com>
2023-09-19 23:21:04 +0100

    Add box that does words and attach it to the player

--- scenes/ui/BoxOfWords.gd
+++ scenes/ui/BoxOfWords.gd
@@ -5,20 +5,17 @@ extends ColorRect
 var dialogue 
 
 func _ready():
-	$Timer.set_wait_time = text_speed
-	sayDialogue()
-	
-func _process(_delta):
-	if (Input.is_action_just_pressed("ui_accept")):
-		pass
-		
+	print_tree_pretty()
+	$FurnitureName.bbcode_text = "This is a name"
+	$Timer.wait_time = text_speed as float
+	sayDialogue()		
 	
 func setDialogue(d: String):
 	dialogue = d
 	
 func sayDialogue():
 	$FurnitureName.bbcode_text = "This is a name"
-	$FurnitureDescription.bbcode_text = "This is a desc"
+	$FurnitureDescription.bbcode_text = "This is a description"
 	$FurnitureDescription.visible_characters = 0
 	while $FurnitureDescription.visible_characters < len($FurnitureDescription.text):
 		$FurnitureDescription.visible_characters += 1
