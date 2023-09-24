65f736cf8f3377d8372f931cc71685b4427e98bb
Holly <honeill31@gmail.com>
2023-09-24 16:56:28 +0100

    Show what room you're in

--- scenes/levels/FurnitureLevel/game_scorer.gd
+++ scenes/levels/FurnitureLevel/game_scorer.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 # Called when the node enters the scene tree for the first time.
+signal player_position
+
 func _ready():
+	pass
+
+func _broadcast_player_group():
 	pass
 	
 func _check_all_groups():
@@ -19,7 +24,11 @@ func _check_group(room: Area2D):
 	#Get the items in the room
 	var score = 0
 	for item in room.get_overlapping_bodies():
+		if item.name == "player":
+			#print("playerrrrrrrr is in: ", room.name)
+			player_position.emit(room.name)
 		#Get groups of item
+		#print("body thing: ",item)
 		var is_correct_group = false
 		for group in item.get_groups():
 			if group == room_type:
@@ -28,10 +37,9 @@ func _check_group(room: Area2D):
 	
 	# Get all the objects of this room type
 	var other_items = get_tree().get_nodes_in_group(room_type)
-	var scaled_score = score/(len(other_items)-1)
-	print("Score: ", scaled_score)
+	var scaled_score = score/(len(other_items))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	#_check_all_groups()
+	#pass
+	_check_all_groups()
